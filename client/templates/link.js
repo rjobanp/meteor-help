47b00c7d59f4de56998211e26666175e0c740f54
Roshan Jobanputra <rosh93@gmail.com>
2014-09-25 15:27:49 -0700

    Use font awesome and add sharing buttons.

--- client/templates/link.js
+++ client/templates/link.js
@@ -12,3 +12,15 @@ Template.linkPage.created = function() {
 Template.linkPage.destroyed = function() {
   document.title = 'Meteor Help - A catalog of content from the best in the Meteor and Javascript community';
 }
+
+Template.linkPage.helpers({
+  encodedUrl: function() {
+    return encodeURI('http://meteorhelp.com/l/' + this.slug);
+  },
+  encodedName: function() {
+    return encodeURIComponent(this.name);
+  },
+  encodedBlurb: function() {
+    return encodeURIComponent('Check out "' + this.name + '" on @MeteorHelp !');
+  }
+});
